4f334d374a6cc9f131007f44828136e96147edaa
dwege <ojorodavid@gmail.com>
2026-02-27 13:30:00 +0300

    making adjustments to the models

--- models/motorbike.py
+++ models/motorbike.py
@@ -1,9 +1,9 @@
 from models.vehicle import Vehicle
 
 class Motorbike(Vehicle):
-    def __init__(self, brand, cost_per_day, engine_capacity):
-        super().__init__(brand, cost_per_day)
+    def __init__(self,id, brand, cost_per_day, engine_capacity):
+        super().__init__(id, brand, cost_per_day)
         self.engine_capacity = engine_capacity
 
     def motorbike_info(self):
-        return f"{self.brand} with {self.engine_capacity} cc costing {self.cost_per_day} per day"
+        return f"{self.brand} of id {self.id} with {self.engine_capacity} cc costing {self.cost_per_day} per day"
